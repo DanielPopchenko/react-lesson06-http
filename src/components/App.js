8a2c18d2f11b8155a60b8132d67f667a6a74e49a
Daniel Popchenko <104201822+DanielPopchenko@users.noreply.github.com>
2023-03-08 20:12:18 +0200

    added required files

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,11 @@
-import React from 'react';
-import Product from './Product';
-import Section from './Section/Section';
-import BookList from './BookList/BookList';
-import favouriteBooks from '../books.json';
+import React, { Component } from "react";
 
-// ! Обязательно компоненты с Большой буквы
-export default function App() {
-  return (
-    <>
-      <BookList books={favouriteBooks} />
-      <h1>Welcome!</h1>
-      <Section title="Best sellings">
-        <Product
-          name="Mersedes"
-          imgUrl="https://www.topgear.com/sites/default/files/images/cars-road-test/2020/06/d327460dc4dcdc379157dec54233dc0c/pcgb20_0355_fine.jpg"
-          price={85400}
-          quantity={30}
-        />
-      </Section>
-
-      <Section title="Recommended">
-        <Product
-          imgUrl="https://www.bmw-m.com/content/dam/bmw/marketBMW_M/common/all-models/m-automobile/3-0-csl/bmw-3-0-csl-stage-teaser.png"
-          name="BMW"
-          price={145000}
-          quantity={100}
-        />
-      </Section>
-
-      <Section title="Your choise">
-        <Product
-          imgUrl="https://cdni.autocarindia.com/Utils/ImageResizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/Continental%20GTModelImage.jpg&w=386&h=256&q=90&c=1"
-          name="Bentley"
-          price={245000}
-          quantity={10}
-        />
-      </Section>
-
-      <Section title="Your choise">
-        <Product
-          imgUrl="https://cdni.autocarindia.com/Utils/ImageResizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/Continental%20GTModelImage.jpg&w=386&h=256&q=90&c=1"
-          name="Bentley"
-          price={125000}
-          quantity={100}
-        />
-      </Section>
-    </>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>App</h1>
+      </div>
+    );
+  }
 }
