4a0a7ba4cf5243d64e65ae21df834e22dee29906
Daniel Popchenko <104201822+DanielPopchenko@users.noreply.github.com>
2023-03-08 20:21:29 +0200

    added prettier and files for task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import App from './App';
 import './base.css';
 
 // ! Метод render не поддерживается, поэтому юзаем новый метод createRoot()!
