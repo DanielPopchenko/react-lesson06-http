c04fdbbb227d4001e3df07ea2082658f20bbeb89
Daniel Popchenko <104201822+DanielPopchenko@users.noreply.github.com>
2023-03-14 17:17:33 +0200

    Сбросил все файлы в 0
    Сессия номер 2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React, { Component } from 'react';
-import PokemonForm from './components/PokemonForm';
-import PokemonInfo from './components/PokemonInfo';
+// import PokemonForm from './components/PokemonForm';
+// import PokemonInfo from './components/PokemonInfo';
 
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
 export default class App extends Component {
-  state = { pokemonName: '' };
-
-  handleFormSubmit = (pokemonName) => {
-    this.setState({ pokemonName });
-    console.log(pokemonName);
-  };
+  // state = { pokemonName: '' };
 
   render() {
-    return (
-      <div style={{ maxWidth: 1170, padding: 20, margin: '0 auto' }}>
-        <PokemonForm onSubmit={this.handleFormSubmit} />
-
-        <PokemonInfo pokemonName={this.state.pokemonName} />
-
-        <ToastContainer autoClose={3000} />
-      </div>
-    );
+    return <div>App</div>;
   }
 }
